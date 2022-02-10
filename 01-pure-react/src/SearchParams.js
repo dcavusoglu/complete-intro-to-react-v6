f22539577b5935bee745b56b4a5b271263c60457
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-02-10 12:56:39 +0300

    eslint hooks plugin

--- 01-pure-react/src/SearchParams.js
+++ 01-pure-react/src/SearchParams.js
@@ -1,11 +1,18 @@
+import {useState} from "react";
+
 const SearchParams = () => {
-  const location = "Seatle, WA";
+  const [location, SetLocation] = useState("Seatle, WA");
 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
-        <input id="location" value={location} placeholder="Location"/>
+        <input
+          id="location"
+          onChange={(e) => SetLocation(e.target.value)}
+          value={location}
+          placeholder="Location"
+        />
         </label>
         <button>Submit</button>
       </form>
